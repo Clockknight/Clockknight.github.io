ced304d8f3000d838668649a0ce2fccdc9353a94
Clockknight <Tylerf2wang@gmail.com>
2020-01-14 11:44:11 -0800

    Beginning of fileRaiser Program

---
+++ file-raiser/fileRaiser.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+raisecount = 0
+
+dirExist = False
+
+directory = ''
+
+while dirExist == False
+    directory = input('Please input a directory to scan for files to raise.')
+
+    #Validate directory's existence
+
+#Use os.walk to find every file in every folder/subfolder
+    #If the file needs to be moved, increase the counter by 1
+    #Only do this if the file isnt already in the directory.
+
+#Display the amount of files that would be raised by the program.
+    #Check with user that the directory and all details of the the directory are correct.
+    #For each file, take it and move it to original directory
