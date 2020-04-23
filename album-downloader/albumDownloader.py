f372a8d6bf885a7f3fc71e6391556efe29e611b7
Clockknight <tylerf2wang@gmail.com>
2020-04-22 23:24:48 -0700

    Started albumDownloader formatting

--- album-downloader/albumDownloader.py
+++ album-downloader/albumDownloader.py
@@ -1,19 +1,23 @@
 import sys
 import beautifulSoup4
 
-#"Cache Mode", uses a text file with pre-written links
+def cacheMode():
     #Find text file with links to google searches of albums' songs
     #Use readlines to seperate out the links of albums
     #Run downloadAlbum
 
-#"Search Mode"
+def searchMode():
     #Search for a songwriter
     #Try to find all albums from them
     #Run downloadAlbum
 
-#downloadAlbum Function
+def downloadAlbum():
     #For each link
         #Find every song inside
         #Take note of song title (Try to leave out extraneous things like numbers)
         #Find respective YT video
         #download them to a directory based on album name
+
+def main():
+
+main()
