1f114846eaf40d7e9ac6c947a7819f7b401abce6
Clockknight <tylerf2wang@gmail.com>
2020-04-23 22:22:45 -0700

    Started on twitterGrabber

---
+++ twitter-grabber/twitterGrabber.py
@@ -0,0 +1,6 @@
+import pyperclip
+
+#Will prompt user for a twitter account
+#Will scrape the account's following page for users
+#Will append '/media\n' at the end of each url, finding media pages
+#Gather all the links together and then use pyperclip to copy all of that onto the user's clipboard
