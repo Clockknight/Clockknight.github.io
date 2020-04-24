0ab08cc41b8e3213a0bb9e7cd0d21e2948217e65
Clockknight <tylerf2wang@gmail.com>
2020-04-23 22:40:02 -0700

    Code is set up to begin webscraping

--- twitter-grabber/twitterGrabber.py
+++ twitter-grabber/twitterGrabber.py
@@ -1,4 +1,31 @@
+import sys
+import bs4
+import requests
 import pyperclip
+
+followedSoup = ''
+userUrl = ''
+
+urlDNE = True
+
+linkList = []
+
+while urlDNE:
+    userUrl = input('Please input the id of the target twitter account.\nFor example, Bernie Sanders would input \"BernieSanders\", not \"Bernie Sanders\".\n')
+    userUrl = 'https://twitter.com/' + userUrl + '/following'
+
+    try:
+        res = requests.get(userUrl)
+        res.raise_for_status()
+        urlDNE = False#If it raises succesfully, the code will stop looping
+
+    except:
+        print("Error.\n" + sys.exc_info()[0])
+
+
+followedSoup = bs4.BeautifulSoup(res.text, 'lxml')
+linkList = followedSoup.select('a')
+
 
 #Will prompt user for a twitter account
 #Will scrape the account's following page for users
