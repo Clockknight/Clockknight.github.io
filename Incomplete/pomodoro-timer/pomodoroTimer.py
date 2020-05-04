5c8069787a24137bf0be3cf845b585fc737f2e9a
Clockknight <tylerf2wang@gmail.com>
2020-05-03 22:53:17 -0700

    Functioning Button

--- Incomplete/pomodoro-timer/pomodoroTimer.py
+++ Incomplete/pomodoro-timer/pomodoroTimer.py
@@ -2,6 +2,17 @@ import sys
 import time
 import tkinter
 
-mainGui = tkinter.Tk(screenName=None,  baseName=None,  className='Tk',  useTk=1)
+switchStatus = 'Start'
+seconds = 900
 
+#Create main GUI for user
+mainGui = tkinter.Tk(screenName=None,  baseName=None,  className='Tk',  useTk=1)
+mainGui.title('Pomodoro Timer')
+
+switchButton = tkinter.Button(mainGui, text=switchStatus, width=25, command=mainGui.destroy)
+switchButton.pack() #Selecting the layout of the button
+
+mainGui = Label(master, option=str(seconds))
+
+#Keep GUI from closing as soon as it starts
 mainGui.mainloop()
