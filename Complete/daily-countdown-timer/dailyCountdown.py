ee17dc2d3f7e1a000b87b2906ee908d44b8196f6
Clockknight <tylerf2wang@gmail.com>
2020-05-04 22:55:36 -0700

    Removed plan text at the head of the script

--- Complete/daily-countdown-timer/dailyCountdown.py
+++ Complete/daily-countdown-timer/dailyCountdown.py
@@ -1,20 +1,3 @@
-'''
-PLAN for the code:
-#Get constants
-    #Get current Year number
-    #Get current Month number
-    #Get current Day number
-
-#Check for a countdownDir.txt in this file's location
-    #If it doesn't exist, move on
-    #If it does, check for the existance of the target
-        #If any of the above fails, prompt for file location. Repeat until everything is okay.
-
-#Once done prompting, move onto updating the file.
-    #Line 31 of the file is Year
-    #Line 32 of the file is Month
-    #Line 33 of the file is Day
-'''
 import os
 import sys
 import datetime
