825ecdf9a66b149e706cc54b10828138aae0f75f
Clockknight <tylerf2wang@gmail.com>
2020-05-04 22:54:36 -0700

    Rearranged files

--- Incomplete/pomodoro-timer/pomodoroTimer.py
+++ Incomplete/pomodoro-timer/pomodoroTimer.py
@@ -9,10 +9,9 @@ seconds = 900
 mainGui = tkinter.Tk(screenName=None,  baseName=None,  className='Tk',  useTk=1)
 mainGui.title('Pomodoro Timer')
 
+#Button to toggle timer
 switchButton = tkinter.Button(mainGui, text=switchStatus, width=25, command=mainGui.destroy)
-switchButton.pack() #Selecting the layout of the button
-
-mainGui = Label(master, option=str(seconds))
+switchButton.pack() #Button Layout
 
 #Keep GUI from closing as soon as it starts
 mainGui.mainloop()
