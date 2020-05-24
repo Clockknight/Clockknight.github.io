91e737dffcde9b7ffa4bc1bd20e03932d0387571
Clockknight <tylerf2wang@gmail.com>
2020-05-24 00:30:53 -0700

    Included functioning button

--- Incomplete/pomodoro-timer/pomodoroTimer.py
+++ Incomplete/pomodoro-timer/pomodoroTimer.py
@@ -2,25 +2,29 @@ import sys
 import time
 import tkinter as tk
 
-switchStatus = 'Start'
-seconds = 900
+#Using time, create variable of "current" time
+now = time.strftime("%H:%M:%S")
 
 class App():
+
+
+
     def __init__(self):
         #Create main GUI for user
         self.root = tk.Tk()
-        self.label = tk.Label(text="%H:%M:%S")
+        #Timer for user to view
+        self.label = tk.Label(text="")
         self.label.pack()
+        #Button to toggle timer
+        self.button = tk.Button(text='Start', command=self.root.destroy)#Change command after things work
+        self.button.pack()
         self.tickUpdate()
         self.root.mainloop()
 
-        #Button to toggle timer
-        switchButton = tk.Button(mainGui, text=switchStatus, width=25, command=mainGui.destroy)
-        switchButton.pack() #Button Layout
-
     def tickUpdate(self):
-        now = time.strftime("%H:%M:%S")
+        #Set timer label to now
         self.label.configure(text=now)
+        #Run this function again every second
         self.root.after(1000, self.tickUpdate)
 
 
