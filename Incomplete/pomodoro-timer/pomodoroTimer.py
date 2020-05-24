a40e7e74fc43395f93ca6e33f5e818d8771f7d47
Clockknight <tylerf2wang@gmail.com>
2020-05-24 01:25:27 -0700

    Made the button toggle between start and stop

--- Incomplete/pomodoro-timer/pomodoroTimer.py
+++ Incomplete/pomodoro-timer/pomodoroTimer.py
@@ -3,11 +3,12 @@ import time
 import tkinter as tk
 
 #Using time, create variable of "current" time
+duration = '00:15:00'
+timeLeft = '00:15:00'
 now = time.strftime("%H:%M:%S")
+startTime = now
 
 class App():
-
-
 
     def __init__(self):
         #Create main GUI for user
@@ -16,16 +17,35 @@ class App():
         self.label = tk.Label(text="")
         self.label.pack()
         #Button to toggle timer
-        self.button = tk.Button(text='Start', command=self.root.destroy)#Change command after things work
+        self.button = tk.Button(text='Start', command=self.startTimer)
         self.button.pack()
+
         self.tickUpdate()
+
         self.root.mainloop()
+
+
+    def startTimer(self):
+        test()
+        self.button.configure(text='Stop', command=self.stopTimer)
+
+    def stopTimer(self):
+        self.button.configure(text='Start', command=self.startTimer)
+
 
     def tickUpdate(self):
         #Set timer label to now
-        self.label.configure(text=now)
+        self.label.configure(text=timeLeft)
         #Run this function again every second
+
         self.root.after(1000, self.tickUpdate)
 
+def test():
+    now = time.strftime("%H:%M:%S")
+    startTime = now
+
+def timerEnd():
+    timeLeft = duration
+    app.focus()
 
 app = App()
