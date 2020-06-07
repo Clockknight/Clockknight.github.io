b7e375be8ccd1851c9837bb4edd569d2ee420b8f
Clockknight <tylerf2wang@gmail.com>
2020-06-07 01:37:55 -0700

    Code now reads and reacts to entrybox input

--- Incomplete/pomodoro-timer/pomodoroTimer.py
+++ Incomplete/pomodoro-timer/pomodoroTimer.py
@@ -2,42 +2,44 @@ import sys
 import time
 import datetime
 import tkinter as tk
+from tkinter import StringVar
 
 #Using time, create variable of "current" time
 
 
 class App():
 
-
     def __init__(self):
+        #Create main GUI for user
+        self.root = tk.Tk()
+        self.inputVariable = tk.StringVar()
+
         #Initializing variables for apps
-        self.now = datetime.datetime.now()
-        self.startTime = self.now
-
         self.timeOver = datetime.timedelta(seconds=0)
-
         self.duration = datetime.timedelta(minutes=15)
         self.timeLeft = self.duration
 
         self.timerActive = False
 
-        #Create main GUI for user
-        self.root = tk.Tk()
         #Timer for user to view
+        self.HEADER1 = tk.Label(text="Time Left")
+        self.HEADER1.pack()
         self.label = tk.Label(text="")
         self.label.pack()
         #Button to toggle timer
         self.button = tk.Button(text='Start', command=self.startTimer)
         self.button.pack()
         #Button to reset timer
-        self.resetButton = tk.Button(text='Reset', command=self.resetDuration)
+        self.resetButton = tk.Button(text='Reset', command=self.resetTimeLeft)
         self.resetButton.pack()
 
         #Textbox to input duration of timer
-        self.durationTextbox = tk.Entry(test='')
+        self.HEADER2 = tk.Label(text="\nInput time in minutes here:")
+        self.HEADER2.pack()
+        self.durationTextbox = tk.Entry(textvariable=self.inputVariable)
         self.durationTextbox.pack()
         #Button to input time in minutes
-        self.inputButton = tk.Button(text='Enter desiredTimer duration', command=self.inputDuration)
+        self.inputButton = tk.Button(text='Input', command=self.inputDuration)
         self.inputButton.pack()
 
         self.tickUpdate()
@@ -45,7 +47,6 @@ class App():
 
     #Timer label functions
     def startTimer(self):
-        self.updateNow()
         self.timerActive = True
         self.button.configure(text='Stop', command=self.stopTimer)
 
@@ -56,8 +57,8 @@ class App():
     def timerEnd(self):
         self.timeLeft = self.duration
         self.timerActive = False
-        self.button.configure(text='Stop Timer', command=self.stopTimer)
-        app.focus()
+        self.button.configure(text='Start', command=self.startTimer)
+        self.focus()
 
     #Variable update functions
     def tickUpdate(self):
@@ -71,12 +72,18 @@ class App():
         else:
             self.timerEnd()
 
-    def updateNow(self):
-        self.now = datetime.datetime.now()
-        self.startTime = self.now
+    #Reset time left to duration
+    def resetTimeLeft(self):
+        self.timeLeft = self.duration
 
-    def resetDuration(self):
-        self.duration = datetime.timedelta(minutes=15)
-
+    #Change duration based on input
+    def inputDuration(self):
+        givenDuration = self.durationTextbox.get()
+        self.durationTextbox.delete(0, len(givenDuration)+1)
+        if givenDuration.isnumeric():
+            print('s')
+            self.timerEnd()
+        else:
+            self.durationTextbox.insert(0, 'Non-numerical input.')
 
 app = App()
