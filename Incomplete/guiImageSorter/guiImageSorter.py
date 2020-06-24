8a6a492c907f4e770054f72b3369b589738e72ea
Clockknight <tylerf2wang@gmail.com>
2020-06-23 21:58:04 -0700

    Made main gui

---
+++ Incomplete/guiImageSorter/guiImageSorter.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import tkinter as tk
+
+class App():
+
+    def __init__(self):
+        self.root = tk.Tk(className='Clockknight\'s Image Sorter')
+        self.root.geometry('1024x576')
+
+        self.root.mainloop()
+
+    def generateButton(self):
+        print('filler')
+
+
+app = App()
