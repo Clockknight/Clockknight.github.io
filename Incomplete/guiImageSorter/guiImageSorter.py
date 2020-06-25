3341d008b1c07f8852989d763aba2e2fb2412e42
Clockknight <tylerf2wang@gmail.com>
2020-06-24 23:45:31 -0700

    Created framework for generateButtons

--- Incomplete/guiImageSorter/guiImageSorter.py
+++ Incomplete/guiImageSorter/guiImageSorter.py
@@ -8,9 +8,18 @@ class App():
         self.root = tk.Tk(className='Clockknight\'s Image Sorter')
         self.root.geometry('1024x576')
 
+        self.dirArray = []
+        self.dirTarget = '\\testDirectory'
         self.root.mainloop()
 
-    def generateButton(self):
+    def generateButtons(self):
+        #Fill array with items
+        #Clear old array
+        self.dirArray = []
+        for root, dir, files in os.walk(self.dirTarget):
+            print(root)
+
+        #Create buttons based on those items
         print('filler')
 
 
