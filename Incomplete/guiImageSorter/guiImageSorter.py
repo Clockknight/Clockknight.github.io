e969e08268042dc88e0e3077ef1176910b2f630b
Clockknight <tylerf2wang@gmail.com>
2020-06-28 00:04:09 -0700

    Made functioning input textbox

--- Incomplete/guiImageSorter/guiImageSorter.py
+++ Incomplete/guiImageSorter/guiImageSorter.py
@@ -11,15 +11,28 @@ class App():
         self.dirArray = []
         self.dirTarget = '.'
 
+        #Textbox to input duration of timer
+        self.dirHeader = tk.Label(text="\nInput target directory here:")
+        self.dirHeader.pack()
+        self.dirTextbox = tk.Entry(textvariable=self.dirTarget)
+        self.dirTextbox.pack()
+        #Button to input time in minutes
+        self.inputButton = tk.Button(text='Input', command=self.inputDirectory)
+        self.inputButton.pack()
+
         self.generateButtons()
         self.root.mainloop()
+
+    def inputDirectory(self):
+        self.dirTarget = self.dirTextbox.get()
+        self.dirTextbox.delete(0, len(givenDuration)+1)
 
     def generateButtons(self):
         #Fill array with items
         #Clear old array
         self.dirArray = []
         for root, dir, files in os.walk(self.dirTarget, topdown=False):
-            print(dir[0])
+            print(dir)
 
         #Create buttons based on those items
         print(self.dirArray)
