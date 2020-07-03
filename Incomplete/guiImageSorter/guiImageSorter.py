f4d689f09b2df27b0f0127d997d203624dd38542
Clockknight <tylerf2wang@gmail.com>
2020-07-02 23:39:38 -0700

    Buttons made from generateButtons now work with targetMove correctly

--- Incomplete/guiImageSorter/guiImageSorter.py
+++ Incomplete/guiImageSorter/guiImageSorter.py
@@ -27,19 +27,18 @@ class App():
         self.generateButtons()
         self.root.mainloop()
 
-    #Function to take directory, then calls generateButton
+    #Function to take new directory, delete old buttons, then call generateButton
     def inputDirectory(self):
         newTarget = self.dirTextbox.get()
         self.dirTarget = newTarget
         self.dirTextbox.delete(0, len(self.dirTarget)+1)
-
 
         #Clear old buttons
         for button in self.buttonArray:
             button.destroy()
         self.generateButtons()
 
-    #Clears all variables and then makes buttons based on subfolders
+    #Clears all variables and then makes buttons based on subdirectories
     def generateButtons(self):
         #Clear old variables
         self.dirArray = []
@@ -55,16 +54,22 @@ class App():
                 #create button labelled with current subfolder
                 self.arrayButton = tk.Button(text=object, height=5, width=15)
                 self.buttonArray.append(self.arrayButton)
-                #Set it to call targetMove with it's label as an extra variable
-                self.arrayButton.configure(command=lambda: self.targetMove(self.arrayButton['text']))
                 self.arrayButton.pack()
 
-    #Move file into button's directory
+        #For loop to configure all directory buttons, once they've been generated
+        for index in range(0, len(self.buttonArray)):
+            #Set it to call targetMove with it's label as an extra variable
+            self.buttonArray[index].configure(command=lambda index=index: self.targetMove(str(self.dirArray[index])))
+
+    #Should move currently selected file into button's target
     def targetMove(self, test):
         print(test)
         #Create buttons based on those items
 
+    #Will begin opening image files on main canvas, and also enable all buttonArray buttons
     def startSorting(self):
-        print('null')
+        for button in self.buttonArray:
+
+            print('null')
 
 app = App()
