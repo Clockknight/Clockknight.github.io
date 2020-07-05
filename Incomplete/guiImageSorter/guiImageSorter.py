84cbceeb4a821706a1763153ee05980fe1221b38
Clockknight <tylerf2wang@gmail.com>
2020-07-05 00:14:49 -0700

    undoAll function now undoes all moves

--- Incomplete/guiImageSorter/guiImageSorter.py
+++ Incomplete/guiImageSorter/guiImageSorter.py
@@ -17,6 +17,7 @@ class App():
         self.targetImage = ''
         self.imageArray = []
         self.okayFileTypes = {'.png', '.jpg', '.gif'}
+        self.moveArray = []
 
         #Groups of elements, will pack under either of these empty labels
         self.elemGroup1 = tk.Label()
@@ -36,22 +37,39 @@ class App():
         self.targetDirLabel.pack()
         self.startButton = tk.Button(self.elemGroup1, text='Start Sorting', command=self.startSorting)
         self.startButton.pack()
+        self.undoButton = tk.Button(self.elemGroup1, text='UNDO ALL MOVES', command=self.undoAll)
+        self.undoButton.pack()
+
         self.skipButton = tk.Button(self.elemGroup2, text='Skip Image', command=self.updateImage,height=5, state='disabled', width=15)
         self.skipButton.pack()
+
+        self.imageLabel = tk.Label()
+        self.imageLabel.pack()
 
         self.generateButtons()
         self.root.mainloop()
 
     #Function to take new directory, delete old buttons, then call generateButton
     def inputDirectory(self):
-        newTarget = self.dirTextbox.get()
-        self.dirTarget = newTarget
-        self.dirTextbox.delete(0, len(self.dirTarget)+1)
-
-        #Clear old buttons
+        self.dirTarget =  self.dirTextbox.get()#Change variable to textbox text
+        self.dirTextbox.delete(0, len(self.dirTarget)+1)#Clear textbox text
+        self.imageLabel.configure(text='')#Clear imageLabel in case it isnt already empty
+        #Clear old buttons, stored in the array
         for button in self.buttonArray:
             button.destroy()
-        self.generateButtons()
+
+        self.generateButtons()#Generate buttons based on new directory
+
+    def undoAll(self):
+        arrayLen = int(len(self.moveArray) / 2)#Get half of length of array (guaranteed to be an even number)
+
+        for index in range(arrayLen):
+            x = index * 2
+            y = x + 1
+            shutil.move(self.moveArray[x], self.moveArray[y])
+
+
+        self.stopSorting()
 
     #Clears all variables and then makes buttons based on subdirectories
     def generateButtons(self):
@@ -79,35 +97,55 @@ class App():
 
     #Will begin opening image files on main canvas, and also enable all buttonArray buttons
     def startSorting(self):
-        #Enable each button in buttonArray
-        for button in self.buttonArray:
-            button.configure(state='normal')
 
         #Select viable images in the directory, by first looking through all images
-        for root, dir, files in os.walk(self.dirTarget, topdown=False):
+        for folder, dir, files in os.walk(self.dirTarget, topdown=False):
             for file in files:
-                #Check file type against filetypes in okayFileTypes dictionary
-                if file[-4:].lower() in self.okayFileTypes:
-                    self.imageArray.append(self.dirTarget + '\\' + file)
+                if folder == self.dirTarget:
+                    #Check file type against filetypes in okayFileTypes dictionary
+                    if file[-4:].lower() in self.okayFileTypes:
+                        self.imageArray.append(self.dirTarget + '\\' + file)
 
-        #Disable the new directory/start sorting button while sorting images
-        self.inputButton.configure(state='disabled')
-        self.startButton.configure(state='disabled')
-        self.skipButton.configure(state='normal')
+        #Only do the following things if the image array isn't empty
+        if len(self.imageArray) > 0:
+            #Disable the new directory/start sorting button while sorting images
+            self.inputButton.configure(state='disabled')
+            self.startButton.configure(state='disabled')
+            self.skipButton.configure(state='normal')
+            for button in self.buttonArray:
+                button.configure(state='normal')
 
-        #Select and display the first available image in the image array
-        self.targetImage = self.imageArray[0]
-        self.targetIndex = 0
-        self.currentImage = tk.PhotoImage(file=self.targetImage)
-        self.imageLabel = tk.Label(image=self.currentImage)
-        self.imageLabel.pack()
+            #Select and display the first available image in the image array
+            self.targetImage = self.imageArray[0]
+            self.targetIndex = 0
+            self.currentImage = tk.PhotoImage(file=self.targetImage)
+            self.imageLabel.configure(text='', image=self.currentImage)
+
+        else:
+            self.imageLabel.configure(text='No images to sort in the given directory!')
+
+    #Function to stop the program once everything has been sorted
+    def stopSorting(self):
+        self.inputButton.configure(state='normal')
+        self.startButton.configure(state='normal')
+
+        self.skipButton.configure(state='disabled')
+        for button in self.buttonArray:
+            button.configure(state='disabled')
 
     #Functions that change currently displayed image
     #Should move currently selected file into button's target
-    def targetMove(self, test):
+    def targetMove(self, inputTarget):
         #Move image file
-        targetFile = self.targetImage
-        shutil.move(targetFile, test)
+        inputTarget = os.path.abspath(inputTarget)
+        targetFile = os.path.abspath(self.targetImage)
+
+        #Get relevant piece of targetFile, then append all strings to moveArray
+        filename = os.path.basename(targetFile)
+        self.moveArray.append(inputTarget + '\\' + filename)
+        self.moveArray.append(targetFile)
+
+        shutil.move(targetFile, inputTarget)
 
         #Update variables and arrays
         del self.imageArray[self.targetIndex]
@@ -124,7 +162,7 @@ class App():
         else:
             self.targetIndex = 0
 
-        if self.imageArrayMax != 0:
+        if self.imageArrayMax != -1:
             self.targetImage = self.imageArray[self.targetIndex]
             self.targetLoad = Image.open(self.targetImage)
 
@@ -132,5 +170,7 @@ class App():
             self.currentImage = ImageTk.PhotoImage(self.targetLoad)
             self.imageLabel.configure(image=self.currentImage)
         else:
+            self.imageLabel.configure(text='No more movable files in this directory!', image='')
+            self.stopSorting()
 
 app = App()
