21ce95fce72027ad2232368a2665380b9b0356fd
Clockknight <tylerf2wang@gmail.com>
2020-07-26 02:50:31 -0700

    Code now pulls href values

--- Incomplete/album-downloader/albumDownloader.py
+++ Incomplete/album-downloader/albumDownloader.py
@@ -6,7 +6,9 @@ from bs4 import BeautifulSoup
 
 #Global Variables
 ua = UserAgent()
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/537.13 (KHTML, like Gecko) Chrome/24.0.1290.1 Safari/537.13'}
 
+#Functions for
 def optionSelect():
     option = input('''
     Welcome to Clockknight's Album Downloader. Please choose from an option below by entering the option number:
@@ -30,7 +32,6 @@ def optionSelect():
     else:
         print('Invalid option selected. Please try again.\n\n')
         optionSelect()
-
 def cacheMode():
     fileDir = ''
     resultArray = []
@@ -45,40 +46,75 @@ def cacheMode():
     downloadAlbum(resultArray)
 
 def searchMode():
+    '''
+    resultCount = 0
     resultLinks = []
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/537.13 (KHTML, like Gecko) Chrome/24.0.1290.1 Safari/537.13'}
+
 
     #Get input for a songwriter
-        #Temporarily listing query as madeon by default
-        #query = 'https://www.google.com/search?q=' + urllib.parse.quote_plus(input('\nPlease input the name of the artist you want to search the discography of.\n\t') + ' albums')
-    query = 'https://www.google.com/search?q=' + urllib.parse.quote_plus('madeon' + '+albums')
+    #Temporarily listing query as madeon by default
+    #searchArtist = input('\nPlease input the name of the artist you want to search the discography of.\n\t')
+    searchArtist = 'madeon'
+    query = 'https://www.google.com/search?q=' + urllib.parse.quote_plus(searchArtist + ' albums')
     #Replace spaces with + to fit google search URL format
 
     #Try to find all albums from them
     try:
         page = requests.get(query, headers=headers)#Use requests on the new URL
         soup = BeautifulSoup(page.text, "html.parser")#Take requests and decode it
-        #for link in soup.find_all('div', {'class':'title'}):
         divList = soup.find_all('div', {'class': 'title'})
         for item in divList:
-                print(item)
+            resultLinks.append(item.contents[0])
+            resultCount += 1
 
-        #Create search links in formula of "artist album songs"
-
+        #Create search links based on results
+        for i in range(0, resultCount):
+            resultLinks[i] = 'https://www.google.com/search?q=' + urllib.parse.quote_plus(resultLinks[i] + ' songs ' + searchArtist)
 
         #Run downloadAlbum
         downloadAlbum(resultLinks)
 
     except TypeError:
-        print('Error:' + TypeError)
+        print('Error:' + str(TypeError))
+    '''
+
+    #TEST FOR DOWNLOAD ALBUM
+    resultLinks = ['https://www.google.com/search?rlz=1C1CHBF_enUS899US899&sxsrf=ALeKk036F02P9rnhODzJf_1lXsLL7IAFYw%3A1595754306716&ei=QkcdX-muK5zC0PEPpLqqyA8&q=+Adventure+songs+Madeon&oq=+Adventure+songs+Madeon&gs_lcp=CgZwc3ktYWIQAzoHCAAQsAMQQzoFCAAQkQI6BggAEAcQHjoCCABQqz5Y4EtguFBoAXAAeACAAVCIAd8BkgEBM5gBAKABAaABAqoBB2d3cy13aXrAAQE&sclient=psy-ab&ved=0ahUKEwjp1-aEyOrqAhUcITQIHSSdCvkQ4dUDCAw&uact=5']
+    downloadAlbum(resultLinks)
 
 def downloadAlbum(givenArray):
-    #For each link
-        #Find every song inside
+    for link in givenArray:
+        #Refresh variables for each link
+        songList = []
+        songCount = 0
+        #try:
+        #Process each link and grab song titles from them
+        page = requests.get(link, headers=headers)#Use requests on the new URL
+        soup = BeautifulSoup(page.text, "html.parser")#Take requests and decode it
+        divList = soup.find_all('div', {'class': 'title'})
+        for item in divList:
+            songList.append(item.contents[0])#Refer to the first item of contents, since .contents returns an array
+            songCount += 1#Keep track of songList length
+
+        #Process each song by appending the song title to the search
+        for song in songList:
+            url = link + urllib.parse.quote_plus( ' ' + song)
+            page = requests.get(url, headers=headers)
+            soup = BeautifulSoup(page.text, "html.parser")
+            resultList = soup.find_all('a', href=True)
+            for result in resultList:
+                if (result['href'][:22] == 'https://www.youtube.com') or (result['href'][7:21] == 'http://www.youtube.com'):
+                    print(ping)
+
+                print(len(resultList))
+
+
+        #except TypeError:
+        #    print('Error:' + str(TypeError.content))
+
         #Take note of song title (Try to leave out extraneous things like numbers)
         #Find respective YT video
         #download them to a directory based on album name
-    print('null')
 
 #Prompt the user for an option
 
