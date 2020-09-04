a48f1a80c67e85c3c92f3afcb44fc21e846208b8
Clockknight <tylerf2wang@gmail.com>
2020-09-03 18:42:20 -0700

    fixed downloadAlbum

--- Incomplete/album-downloader/albumDownloader.py
+++ Incomplete/album-downloader/albumDownloader.py
@@ -53,6 +53,7 @@ def cacheMode():
 def searchMode():
     resultCount = 0
     resultLinks = []
+    temp = ''
 
     #Get input for a songwriter
     artist = input('\nPlease input the name of the artist you want to search the discography of.\n\t')
@@ -63,38 +64,39 @@ def searchMode():
     query = 'https://www.google.com/search?q=discogs+' + searchArtist
     try:
         page = requests.get(query, headers=headers)#Use requests on the new URL
-        soup = BeautifulSoup(page.text, "html.parser")#Take requests and decode it
-        aList = soup.find_all('a', href=True)
-        for item in aList:
-            if item['href'][:30] == 'https://www.discogs.com/artist':
-                temp = item['href'] + '?type=Releases&subtype=Albums&filter_anv=0'
-                print('Going to page ' + temp + '...')
-                break#Take the first viable link and then process it.
-    except:
-        print('Error:')
-
-    #Find album name and find google results based on previous inputs & results
-    try:
-        albumCheck = artist + ' - '#Create string to check against later
-        page = requests.get(temp, headers=headers)#Use requests on the new URL
-
-
     except:
         print('Error:')
 
     soup = BeautifulSoup(page.text, "html.parser")#Take requests and decode it
+    aList = soup.find_all('a', href=True)
+    for item in aList:
+        if item['href'][:30] == 'https://www.discogs.com/artist':
+            temp = item['href'] + '?type=Releases&subtype=Albums&filter_anv=0'
+            print('Going to page ' + temp + '...')
+            break#Take the first viable link and then process it.
+
+    #Find album name and find google results based on previous inputs & results
+    albumCheck = artist#Create string to check against later
+    page = requests.get(temp, headers=headers)#Use requests on the new URL
+
+    soup = BeautifulSoup(page.text, "html.parser")#Take requests and decode it
     imgList = soup.find_all('img', alt=True)#Make array of img tags with alt
     for item in imgList:
-        if item['alt'][:searchLen+3] == albumCheck:#Check alt text against albumCheck string, based on Discogs formatting
+        print('ping')
+        print(item['alt'][:searchLen])
+        print(artist)
+        if item['alt'][:searchLen] == artist:#Check alt text against artist String, only taking enough charcters to match
             #Convert all info into one google search URL
             temp = 'https://www.google.com/search?q=' + urllib.parse.quote_plus(artist + ' ' +  item['alt'][searchLen+3:-10]) + '+songs'
             resultLinks.append(temp)#Add Link to array
+            print(resultLinks)
 
     downloadAlbum(resultLinks)#Call download Album with all songs wanted
 
 def downloadAlbum(givenArray):
     blacklist = '.\'/\\' #String of characters that cannot be in filenames
     skippedList = []
+    print('ping')
 
     #LEVEL: Processing google search links
     for link in givenArray:
@@ -118,129 +120,126 @@ def downloadAlbum(givenArray):
             soup = BeautifulSoup(page.text, "html.parser")#Take requests and decode it
 
         except:
-            print('Failure to successfully get HTML info for page containing the album\'s songs!\nShutting down...')
+            print('Failure to successfully get HTML info for page containing the album\'s songs!\nSkipping Album...')
 
+        #Get album and artist title to save as folder
+        searchInput = soup.find('title')
+        searchInput = searchInput.contents[0][:-22]
 
+        #Ask user for input regarding the artist's name in the search
+        infoList = searchInput.split()
+        for item in infoList:
+            print(item)
 
+        #Processing search query to find album and artist names
+        artistName = searchParse(infoList)
+        artistName = artistName.capitalize()
+        #Let user know about album info
+        print('Artist name: ' + artistName)
+        #Update infoList to exclude the artist name
+        for word in infoList[len(artistName.split()):]:
+            albumName += word.capitalize() + ' '
+        print('Album name: ' + albumName + '\n')
+        #Create folder based on album info
+        dirStorage = artistName + ' - ' + albumName
+        dirStorage = dirStorage[:-1]#Remove whitespace at the end of the string
+        os.makedirs(dirStorage, exist_ok=True)#Make the folder
+        print('Creating folder:' + dirStorage)
 
-            #Get album and artist title to save as folder
-            searchInput = soup.find('title')
-            searchInput = searchInput.contents[0][:-22]
+        divList = soup.find_all('div', {'class': 'title'})
+        for div in divList:
+            songList.append(div.contents[0])#Refer to the first item of contents, since .contents returns an array
 
-            #Ask user for input regarding the artist's name in the search
-            infoList = searchInput.split()
-            for item in infoList:
-                print(item)
+        #LEVEL: Processing entire albums
+        #Process each song by appending the song title to the search
+            songIndex = songCount
+            for item in songList:
+                songName = item
+                print('\nSong name: ' + songName)
+                songIndex -= 1
+                #temp stores each song's google search video results
+                temp = 'https://www.google.com/search?q=' + urllib.parse.quote_plus(item) + '+' + link[32:] + '&tbm=vid'
+                try:
+                    page = requests.get(temp, headers=headers)#Use requests on the new URL
+                    soup = BeautifulSoup(page.text, "html.parser")#Take requests and decode it
 
-            #Processing search query to find album and artist names
-            artistName = searchParse(infoList)
-            artistName = artistName.capitalize()
-            #Let user know about album info
-            print('Artist name: ' + artistName)
-            #Update infoList to exclude the artist name
-            for word in infoList[len(artistName.split()):]:
-                albumName += word.capitalize() + ' '
-            print('Album name: ' + albumName + '\n')
-            #Create folder based on album info
-            dirStorage = artistName + ' - ' + albumName
-            dirStorage = dirStorage[:-1]#Remove whitespace at the end of the string
-            os.makedirs(dirStorage, exist_ok=True)#Make the folder
-            print('Creating folder:' + dirStorage)
+                except:
+                    print('Failure to successfully get HTML info for youtube result page for current song!\nShutting down...')
 
-            divList = soup.find_all('div', {'class': 'title'})
-            for div in divList:
-                songList.append(div.contents[0])#Refer to the first item of contents, since .contents returns an array
+                #Look for links in search results
+                aList = soup.find_all('a', href=True)
 
-            #LEVEL: Processing entire albums
-            #Process each song by appending the song title to the search
-                songIndex = songCount
-                for item in songList:
-                    songName = item
-                    print('\nSong name: ' + songName)
-                    songIndex -= 1
-                    #temp stores each song's google search video results
-                    temp = 'https://www.google.com/search?q=' + urllib.parse.quote_plus(item) + '+' + link[32:] + '&tbm=vid'
-                    try:
-                        page = requests.get(temp, headers=headers)#Use requests on the new URL
-                        soup = BeautifulSoup(page.text, "html.parser")#Take requests and decode it
+                #LEVEL: Processing results for each song
+                for a in aList:
+                    if a['href'][:29] == 'https://www.youtube.com/watch':
+                        matchIndex = 0
+                        titleDict = {}
 
-                    except:
-                        print('Failure to successfully get HTML info for youtube result page for current song!\nShutting down...')
+                        #Check title for matching song title
+                        try:
+                            #Beautiful soup the page, to check the title of the video
+                            page = requests.get(a['href'], headers=headers)
+                            soup = BeautifulSoup(page.text, "html.parser")
+                            nameTag = soup.find('meta', {'name': 'title'})
 
-                    #Look for links in search results
-                    aList = soup.find_all('a', href=True)
+                        except:
+                            print('Failure to successfully get HTML info for page containing possible youtube results for current song!\nShutting down...')
 
-                    #LEVEL: Processing results for each song
-                    for a in aList:
-                        if a['href'][:29] == 'https://www.youtube.com/watch':
-                            matchIndex = 0
-                            titleDict = {}
+                        videoTitle = nameTag['content']#store the title of the video in this variable
 
-                            #Check title for matching song title
+                        songNameList = songName.split()#Make an array of words from the song title for later
+
+                        #Add each word in the video title to a dictionary
+                        for word in videoTitle.split():
+                            titleDict[word] = True
+
+                        #Compare words in the song title to words in the dictionary. All words should be present, otherwise don't download the video.
+                        for word in songNameList:
+                            if word in titleDict:
+                                matchIndex += 1
+
+                        #If everything checks out, download the video then break from the loop, moving onto the next song
+                        if matchIndex == len(songNameList):
+                            print('Saved as: ' + songName)#Print the title of the video for the user to know what files to look for.
+                            temp = a['href']
+
+                            scrubbedName = songName
+                            for char in blacklist:
+                                scrubbedName = scrubbedName.replace(char, '')
+
+                            #TODO Add try except case for json.decoder.JSONDecodeError
                             try:
-                                #Beautiful soup the page, to check the title of the video
-                                page = requests.get(a['href'], headers=headers)
-                                soup = BeautifulSoup(page.text, "html.parser")
-                                nameTag = soup.find('meta', {'name': 'title'})
+                                songCount += 1
+                                YouTube(temp).streams.first().download(filename=scrubbedName)
 
-                            except:
-                                print('Failure to successfully get HTML info for page containing possible youtube results for current song!\nShutting down...')
-
-                            videoTitle = nameTag['content']#store the title of the video in this variable
-
-                            songNameList = songName.split()#Make an array of words from the song title for later
-
-                            #Add each word in the video title to a dictionary
-                            for word in videoTitle.split():
-                                titleDict[word] = True
-
-                            #Compare words in the song title to words in the dictionary. All words should be present, otherwise don't download the video.
-                            for word in songNameList:
-                                if word in titleDict:
-                                    matchIndex += 1
-
-                            #If everything checks out, download the video then break from the loop, moving onto the next song
-                            if matchIndex == len(songNameList):
-                                print('Saved as: ' + songName)#Print the title of the video for the user to know what files to look for.
-                                temp = a['href']
-
-                                scrubbedName = songName
-                                for char in blacklist:
-                                    scrubbedName = scrubbedName.replace(char, '')
-
-                                #TODO Add try except case for json.decoder.JSONDecodeError
-                                try:
-                                    songCount += 1
-                                    YouTube(temp).streams.first().download(filename=scrubbedName)
-
-                                except JSONDecodeError:
-                                    print('Song download error! Skipped song: ' + songName)
-                                    skippedList.append(dirStorage + songName)
-                                    pass
+                            except JSONDecodeError:
+                                print('Song download error! Skipped song: ' + songName)
+                                skippedList.append(dirStorage + songName)
+                                pass
 
 
-                                #Create versions of the variables with blacklisted characters removed
-                                targetFile = os.path.join('.\\', scrubbedName + '.mp4')
+                            #Create versions of the variables with blacklisted characters removed
+                            targetFile = os.path.join('.\\', scrubbedName + '.mp4')
 
-                                #Change file into an mp3
-                                targetFile = convertFile(targetFile)
-                                print(targetFile)
+                            #Change file into an mp3
+                            targetFile = convertFile(targetFile)
+                            print(targetFile)
 
-                                tagTarget = eyed3.load(targetFile)
-                                tagTarget.tag.title = songName
-                                tagTarget.tag.artist = artistName
-                                tagTarget.tag.album = albumName
-                                tagTarget.tag.album_artist = artistName
-                                tagTarget.tag.track_num = songCount
-                                #TODO Implement adding album art
-                                tagTarget.tag.save()
+                            tagTarget = eyed3.load(targetFile)
+                            tagTarget.tag.title = songName
+                            tagTarget.tag.artist = artistName
+                            tagTarget.tag.album = albumName
+                            tagTarget.tag.album_artist = artistName
+                            tagTarget.tag.track_num = songCount
+                            #TODO Implement adding album art
+                            tagTarget.tag.save()
 
-                                break#Move onto processing next song
+                            break#Move onto processing next song
 
-                #Move all mp3s in the current working directory to the album folder
-                for file in os.listdir('.\\'):
-                    if file[-4:] == '.mp3':
-                        shutil.move('.\\' + file, '.\\' + dirStorage + '\\' + file)
+            #Move all mp3s in the current working directory to the album folder
+            for file in os.listdir('.\\'):
+                if file[-4:] == '.mp3':
+                    shutil.move('.\\' + file, '.\\' + dirStorage + '\\' + file)
 
 
 #Converts given mp4 file into an mp3 file
