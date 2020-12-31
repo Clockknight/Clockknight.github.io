41203349e973fbdcfa424a4f2af71f6bdf8680fb
Clockknight <34818025+Clockknight@users.noreply.github.com>
2020-12-30 22:15:45 -0800

    Created psuedocode for prototype

---
+++ Incomplete/tf-arbitrage/tfArbitrage.py
@@ -0,0 +1,9 @@
+
+#Function - Main
+#   Call scraping function
+#   Write down values given from function in console
+
+#Function - Scraping scraptf listings
+#   Find listed price on site
+#   Interact with backpack.tf API to find suggested price
+#   Return arrays of prices
