8d1051a72c99e1e7ac67a773cfea636ccaabb6c7
Clockknight <34818025+Clockknight@users.noreply.github.com>
2021-05-03 11:04:19 -0700

    dd

--- Incomplete/tf-arbitrage/tfArbitrage.py
+++ Incomplete/tf-arbitrage/tfArbitrage.py
@@ -1,39 +1,36 @@
-import steampy
-import requests
+import time
+from steampy.client import SteamClient
 
-#global variable to find csv file
+# Set API key
+api_key = '13C314DA0B61D2A625935F856F1F9958'
+# Set path to SteamGuard file
+steamguard_path = '..\Steamguard.txt'
+# Steam username
+username = 'Clockknight'
+# Steam password
+password = 'Tyler69here413'
+
 
 def main():
-    deals = []
-    scrapScrape("http://scrap.tf")
+    print('This is the chat bot.')
+    if not are_credentials_filled():
+        print('You have to fill the credentials to run the example')
+        print('Terminating bot')
+        return
+    client = SteamClient(api_key)
+    client.login(username, password, steamguard_path)
+    print('Bot logged in successfully, polling messages every 10 seconds')
+    while True:
+        time.sleep(10)
+        messages = client.chat.fetch_messages()['received']
+        for message in messages:
+            client.chat.send_message(message['partner'], "Got your message: " + message['message'])
 
-    for deal in deals:
-        print(deal)
-#Function - Main
-#   Call scraping function
-#   Write down values given from function in console
 
-def scrapScrape(url):
-    listingData #Multi-dimension array to store listings + prices + conditions
+def are_credentials_filled() -> bool:
+    return api_key != '' and steamguard_path != '' and username != '' and password != ''
 
-    return someStuff
-#Function - Scraping scraptf listings
-#   Find listed price on site
-#   Interact with backpack.tf API to find suggested price
-#   Return arrays of prices
 
-def bptfPricing(inputArray):
-    checkList#Array of true and falses to correspond with values in inputArray
-
-    return checkList
-#Function - Compare given items & pricing to bptf pricing
-#   Use switch case to find correct item quality
-#   Use ID and correct quality to find correct price of item
-#   Return listof trues and falses
-
-def sendTrade(itemId)
-
-#Function - Send trade offer to profile
-#   Find target to trade with
-#   Use steampy to manage inventory and send offer
-main()
+if __name__ == "__main__":
+    # execute only if run as a {value for value in variable}ript
+    main()
