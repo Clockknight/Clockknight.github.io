32d2a3003d219e29f693a781454e301acc93a53b
Clockknight <34818025+Clockknight@users.noreply.github.com>
2021-07-22 02:41:07 -0700

    Code now successsfully tries to log into steam

--- Incomplete/tf-arbitrage/seleniumAndBS4.py
+++ Incomplete/tf-arbitrage/seleniumAndBS4.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 
@@ -46,32 +47,13 @@ sa.remove()  # removes the authenticator from the account
 browser = webdriver.Chrome()
 browser.maximize_window()
 browser.get("https://scrap.tf/buy/hats")
-wait = WebDriverWait(browser, 10)
+wait = WebDriverWait(browser, 2)
 wait.until(EC.element_to_be_clickable((By.ID, "steamAccountName"))).send_keys(username)
 wait.until(EC.element_to_be_clickable((By.ID, "steamPassword"))).send_keys(password)
-wait.until(EC.element_to_be_clickable((By.ID, "twofactorcode_entry"))).send_keys('12345')
-wait.until(EC.element_to_be_clickable((By.XPATH, "(//div[text()='Submit'])[6]"))).click()
+wait.until(EC.element_to_be_clickable((By.ID, "twofactorcode_entry"))).send_keys(authcode)
+wait.until(EC.element_to_be_clickable((By.ID, "twofactorcode_entry"))).send_keys(Keys.RETURN)
 
 
-'''
-#Using selenium to log in and then get the page
-browser = webdriver.Chrome()
-browser.maximize_window()
-browser.implicitly_wait(30)
-browser.get('https://scrap.tf/buy/hats')
-#Log into steam then proceed to new page
-userbox = browser.find_element_by_name("username")
-userbox.send_keys(username)
-passbox = browser.find_element_by_name("password")
-passbox.send_keys(password)
-signin = browser.find_element_by_id("imageLogin")
-signin.click()
-
-
-wait = WebDriverWait(browser, 10)
-wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input#authcode"))).send_keys(authcode)
-wait.until(EC.element_to_be_clickable((By.XPATH, "(//div[text()='Submit']/..)[6]"))).click()
-'''
 
 #Load listings from site
 #WebDriverWait(browser, delay).until(EC.presence_of_element_located(browser.find_element_by_id('...')))
