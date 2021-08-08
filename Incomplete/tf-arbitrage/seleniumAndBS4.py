77b3f0181934e09ca65597dd495b21c8da670934
Clockknight <34818025+Clockknight@users.noreply.github.com>
2021-08-08 02:32:04 -0700

    Selenium script gets steam authentication code

--- Incomplete/tf-arbitrage/seleniumAndBS4.py
+++ Incomplete/tf-arbitrage/seleniumAndBS4.py
@@ -1,66 +1,55 @@
+import bs4
+import json
 import time
-import requests, bs4
+import requests
+from steampy.guard import generate_one_time_code
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-
-
-
 #Load variables from other files
 infoFile = open(".\info.txt", "r")
 infoArray = infoFile.readlines()
 # Set API
-api_key = infoArray[1]
-# Steam username
-username = infoArray[3]
+api_key = infoArray[1][:-1]
+# Steam steamname
+steamname = str(infoArray[3][:-1])
 # Steam password
-password = infoArray[5]
-authcode = "FAKEG"
+password = str(infoArray[5][:-1])
+# steam's Shared Secret
+secret = infoArray[7]
+delay = 5
 
-#Define other Variables
-delay = 1
-
-'''
-#Trying out "steam" library to get authentication
-wa = MobileWebAuth('steamuser')
-wa.cli_login()
-
-sa = SteamAuthenticator(backend=wa)
-sa.add()    # SMS code will be send to the account's phone number
-sa.secrets  # dict with authenticator secrets (SAVE THEM!!)
-
-# save the secrets, for example to a file
-json.dump(sa.secrets, open('./mysecrets.json', 'w'))
-
-# HINT: You can stop here and add authenticator on your phone.
-#       The secrets will be the same, and you will be able to
-#       both use your phone and SteamAuthenticator.
-
-sa.finalize('SMS CODE')  # activate the authenticator
-sa.get_code()  # generate 2FA code for login
-sa.remove()  # removes the authenticator from the account
-'''
-
-browser = webdriver.Chrome()
-browser.maximize_window()
-browser.get("https://scrap.tf/buy/hats")
-wait = WebDriverWait(browser, 2)
-wait.until(EC.element_to_be_clickable((By.ID, "steamAccountName"))).send_keys(username)
-wait.until(EC.element_to_be_clickable((By.ID, "steamPassword"))).send_keys(password)
-wait.until(EC.element_to_be_clickable((By.ID, "twofactorcode_entry"))).send_keys(authcode)
-wait.until(EC.element_to_be_clickable((By.ID, "twofactorcode_entry"))).send_keys(Keys.RETURN)
+def getAuthCode():
+    steamPyAuthCode = generate_one_time_code(secret)
+    return steamPyAuthCode
 
 
+#Logs into steam, given correct authcode
+def seleniumLogin(authCode):
+    #Browser setup
+    browser = webdriver.Chrome()
+    browser.maximize_window()
+    browser.get("https://scrap.tf/buy/hats")
+    wait = WebDriverWait(browser, delay)
+    wait.until(EC.element_to_be_clickable((By.ID, "steamAccountName"))).send_keys(steamname)
+    wait.until(EC.element_to_be_clickable((By.ID, "steamPassword"))).send_keys(password)
+    wait.until(EC.element_to_be_clickable((By.ID, "steamPassword"))).send_keys(Keys.RETURN)
+    wait.until(EC.element_to_be_clickable((By.ID, "twofactorcode_entry"))).send_keys(authCode)
+    wait.until(EC.element_to_be_clickable((By.ID, "twofactorcode_entry"))).send_keys(Keys.RETURN)
 
-#Load listings from site
-#WebDriverWait(browser, delay).until(EC.presence_of_element_located(browser.find_element_by_id('...')))
+    #Load listings from site
+    #WebDriverWait(browser, delay).until(EC.presence_of_element_located(browser.find_element_by_id('...')))
 
+    #Use soup on finished page
+    soup = bs4.BeautifulSoup(browser.page_source, "html.parser")
+    elements = soup.select('')
+    print(elements)
 
+def main():
+    authCode = getAuthCode()
+    seleniumLogin(authCode)
 
-#Use soup on finished page
-soup = bs4.BeautifulSoup(browser.page_source, "html.parser")
-elements = soup.select('')
-print(elements)
+main()
