d6f2da68e547d8f18c94214f79d0d3e848125ee5
Clockknight <tylerf2wang@gmail.com>
2021-08-29 03:03:05 -0700

    Selenium and BS4 Working as intended

--- Incomplete/tf-arbitrage/seleniumAndBS4.py
+++ Incomplete/tf-arbitrage/seleniumAndBS4.py
@@ -1,8 +1,15 @@
-import bs4
-import json
-import time
+import os
 import requests
+import time
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+import steampy
 from steampy.guard import generate_one_time_code
+from steampy.client import SteamClient
+import openpyxl
+from openpyxl import Workbook
+from openpyxl import load_workbook
+import selenium
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -28,8 +35,7 @@ def getAuthCode():
 
 
 #Logs into steam, given correct authcode
-def scraptfScan(authCode):
-    elements = []
+def seleniumLogin(authCode):
 
     #Browser setup
     browser = webdriver.Chrome()
@@ -42,29 +48,41 @@ def scraptfScan(authCode):
     wait.until(EC.element_to_be_clickable((By.ID, "twofactorcode_entry"))).send_keys(authCode)
     wait.until(EC.element_to_be_clickable((By.ID, "twofactorcode_entry"))).send_keys(Keys.RETURN)
 
-    #Load listings from site
-    time.sleep(delay)
-    browser.get("https://scrap.tf/buy/hats")
+    #Variable declaration
+    elements = []
+    file = 'tfArbitrage.xlsx'
+    wb = load_workbook(filename=file)
+    ws = wb.active
+
     time.sleep(delay)
     #Use soup on finished page
-    soup = bs4.BeautifulSoup(browser.page_source, "html.parser")
+    browser.get("https://scrap.tf/buy/hats")
+    soup = BeautifulSoup(browser.page_source, "html.parser")
     elementContainers = soup.find_all('div', class_="items-container")
     for container in elementContainers:#Site has all items in divs in item container classes, this selects all of them
         for element in container.find_all('div'):#Tracks down all divs in the results
             dataID = element.get('data-id')#Only processes divs with a data-id attribute
             if (dataID != None and element.get('data-content') != "&lt;b&gt;This item is overstocked and cannot be sold.&lt;/b&gt;"):
-                elemData = [] #Refresh elemData variable, to store information
+                elemData = [] #Refresh elemData variable, to store information on each specific item
                 elemData.append(dataID)#Item ID Number
                 elemData.append(element.get('data-title'))#Item Name
-                elemData.append(element['class'])#Item Quality Number
+                elemData.append(element['class'][2])#Item Quality Number
                 elemData.append(element.get('data-bot23-count'))#Num of item available
                 elemData.append(element.get('data-content'))#Item content, incl. Cost
-                elements.append(elemData) #Should select each item
+                if elemData[4] != '':
+                    elements.append(elemData) #Should select each item
 
-    for elem in elements:
-        print(elem)
+
+
+    #declare info about the sheet here
+    for i in range(0, len(elements)):
+        for j in range(0, len(elements[i])):
+          ws.cell(row=i+2, column=j+1).value = str(elements[i][j])#Add 1 to i, so it has space to work with
+
+    wb.save(file)
 
 def main():
-    scraptfScan(getAuthCode())
+    authCode = getAuthCode()
+    seleniumLogin(authCode)
 
 main()
