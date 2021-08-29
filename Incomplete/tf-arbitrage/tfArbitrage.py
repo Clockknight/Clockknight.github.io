fd6c41c0a617e7cfb31764a06489588f815b8828
Clockknight <tylerf2wang@gmail.com>
2021-08-29 03:16:11 -0700

    Merged test script with primary script

--- Incomplete/tf-arbitrage/tfArbitrage.py
+++ Incomplete/tf-arbitrage/tfArbitrage.py
@@ -1,11 +1,20 @@
 import os
+import time
+import requests
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+import steampy
+from steampy.guard import generate_one_time_code
+from steampy.client import SteamClient
 import openpyxl
 from openpyxl import Workbook
-import requests
-from bs4 import BeautifulSoup
-import time
-from steampy.client import SteamClient
-from fake_useragent import UserAgent
+from openpyxl import load_workbook
+import selenium
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 #Function definitions
@@ -21,6 +30,57 @@ def infoCreate():
     #Put shared secret on line below''')
     infoFile.close()
 
+def getAuthCode():
+    steamPyAuthCode = generate_one_time_code(secret)
+    return steamPyAuthCode
+
+    #Logs into steam, given correct authcode
+def seleniumLogin(authCode):
+
+    #Browser setup
+    browser = webdriver.Chrome()
+    browser.maximize_window()
+    browser.get("https://scrap.tf/buy/hats")
+    wait = WebDriverWait(browser, delay)
+    wait.until(EC.element_to_be_clickable((By.ID, "steamAccountName"))).send_keys(steamname)
+    wait.until(EC.element_to_be_clickable((By.ID, "steamPassword"))).send_keys(password)
+    wait.until(EC.element_to_be_clickable((By.ID, "steamPassword"))).send_keys(Keys.RETURN)
+    wait.until(EC.element_to_be_clickable((By.ID, "twofactorcode_entry"))).send_keys(authCode)
+    wait.until(EC.element_to_be_clickable((By.ID, "twofactorcode_entry"))).send_keys(Keys.RETURN)
+
+    #Variable declaration
+    elements = []
+    file = 'tfArbitrage.xlsx'
+    wb = load_workbook(filename=file)
+    ws = wb.active
+
+    time.sleep(delay)
+    #Use soup on finished page
+    browser.get("https://scrap.tf/buy/hats")
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    elementContainers = soup.find_all('div', class_="items-container")
+    for container in elementContainers:#Site has all items in divs in item container classes, this selects all of them
+        for element in container.find_all('div'):#Tracks down all divs in the results
+            dataID = element.get('data-id')#Only processes divs with a data-id attribute
+            if (dataID != None and element.get('data-content') != "&lt;b&gt;This item is overstocked and cannot be sold.&lt;/b&gt;"):
+                elemData = [] #Refresh elemData variable, to store information on each specific item
+                elemData.append(dataID)#Item ID Number
+                elemData.append(element.get('data-title'))#Item Name
+                elemData.append(element['class'][2])#Item Quality Number
+                elemData.append(element.get('data-bot23-count'))#Num of item available
+                elemData.append(element.get('data-content'))#Item content, incl. Cost
+                if elemData[4] != '':
+                    elements.append(elemData) #Should select each item
+
+
+
+    #declare info about the sheet here
+    for i in range(0, len(elements)):
+        for j in range(0, len(elements[i])):
+          ws.cell(row=i+2, column=j+1).value = str(elements[i][j])#Add 1 to i, so it has space to work with
+
+    wb.save(file)
+
 #Creates spreadsheet for data storage
 def sheetCreate():
     print("--Creating tfArbitrage.xlsx in current directory")
@@ -35,44 +95,10 @@ def sheetCreate():
 
     workbook.save(filename="tfArbitrage.xlsx")
 
-    listingSearch()
+    progStart()
 
-#Will look through websites and note all items listed
-def listingSearch():
-    print(api_key)
-    print("-- Beginning search for discounted items.")
-    try:
-        print("-- searching scrap.tf for discounted items")
-
-
-        scrapurl = 'https://scrap.tf/buy/hats'
-
-        #Checking for url error codes
-        req = requests.get(scrapurl)
-        print(req.status_code)
-
-        page = requests.get(scrapurl, headers=headers)
-        soup = BeautifulSoup(page.text, "html.parser")
-        print(soup)
-    except:
-        print('-- scraptf Fail Case.')
-    '''
-        try:
-            print("-- searching backpack.tf for discounted items")
-
-            bpurl = 'https://scrap.tf/buy/hats'
-
-            #Checking for url error codes
-            req = requests.get(scrapurl)
-            print(req.status_code)
-
-            page = requests.get(scrapurl)
-            soup = BeautifulSoup(page.text, "html.parser")
-        except:
-            print('-- bptf Fail Case)
-    '''
-    listingPosts()
-
+'''
+#CURRENTLY UNIMPLEMENTED
 #Will create listings for each item considered "Viable" on the spreadsheet
 def listingPosts():
     print("-- Posting listings for inventory.")
@@ -83,10 +109,15 @@ def listingPosts():
 def tradeBot():
     print("-- Managing incoming trade offers and messages.")
     listingPosts()
+'''
+
+def progStart():
+    authCode = getAuthCode()
+    seleniumLogin(authCode)
 
 def main():
     if(os.path.exists(sheetDirectory)):
-        listingSearch()
+        progStart()
     else:
         sheetCreate()
 
@@ -94,6 +125,7 @@ def main():
 #User Agent Variables
 ua = UserAgent()
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/537.13 (KHTML, like Gecko) Chrome/24.0.1290.1 Safari/537.13'}
+sheetDirectory = ".\tfArbitrage.xlsx"
 
 #SteamPy Variables
 #Read info.txt
@@ -101,18 +133,17 @@ infoDirectory = ".\info.txt"
 if(os.path.exists(infoDirectory) != True):
     infoCreate()
 
-infoFile = open(infoDirectory, "r")
+#Load variables from info file
+infoFile = open(".\info.txt", "r")
 infoArray = infoFile.readlines()
 # Set API
-api_key = infoArray[1]
-# Steam username
-username = infoArray[3]
+api_key = infoArray[1][:-1]
+# Steam steamname
+steamname = str(infoArray[3][:-1])
 # Steam password
-password = infoArray[5]
-# Set path to SteamGuard file
-steamguard_path = '..\Steamguard.txt'
-
-
-sheetDirectory = ".\tfArbitrage.xlsx"
+password = str(infoArray[5][:-1])
+# steam's Shared Secret
+secret = infoArray[7]
+delay = 5
 
 main()
